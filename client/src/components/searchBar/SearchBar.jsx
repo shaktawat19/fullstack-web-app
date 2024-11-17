fc6055742fd05cc5617635dfe4b00ea6e4a550d5
shaktawat19 <harshshaktawat.tech@gmail.com>
2024-11-17 13:46:15 +0530

    Home page completion

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./SearchBar.css";
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
-export default SearchBar
+  const handleSearchSubmit = () => {
+    if (searchTerm.trim()) {
+      navigate(`/search?q=${searchTerm}`);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearchSubmit();
+    }
+  };
+
+  return (
+    <div className="search-bar-container">
+      <input
+        type="text"
+        placeholder="Search"
+        className="search-bar"
+        value={searchTerm}
+        onKeyDown={handleKeyDown}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <i className="fa fa-search search-icon"></i>
+    </div>
+  );
+};
+
+export default SearchBar;
