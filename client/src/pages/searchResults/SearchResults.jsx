240487f5bdc617aec9e79fc7b84325f5ae2cb63f
shaktawat19 <harshshaktawat.tech@gmail.com>
2024-11-18 18:45:34 +0530

    default data

--- client/src/pages/searchResults/SearchResults.jsx
+++ client/src/pages/searchResults/SearchResults.jsx
@@ -5,107 +5,107 @@ import "./SearchResults.css";
 import Navbar from "../../components/navbar/Navbar";
 import SearchBar from "../../components/searchBar/SearchBar";
 
-// const data = [
-//   {
-//     first_name: "Amit",
-//     last_name: "Sharma",
-//     city: "Mumbai",
-//     contact_number: "9876543210",
-//   },
-//   {
-//     first_name: "Amit",
-//     last_name: "Verma",
-//     city: "Delhi",
-//     contact_number: "9123456789",
-//   },
-//   {
-//     first_name: "Sandeep",
-//     last_name: "Gupta",
-//     city: "Bangalore",
-//     contact_number: "9876549870",
-//   },
-//   {
-//     first_name: "Sandeep",
-//     last_name: "Kumar",
-//     city: "Hyderabad",
-//     contact_number: "9871234567",
-//   },
-//   {
-//     first_name: "Priya",
-//     last_name: "Mehta",
-//     city: "Ahmedabad",
-//     contact_number: "9874561230",
-//   },
-//   {
-//     first_name: "Priya",
-//     last_name: "Chopra",
-//     city: "Pune",
-//     contact_number: "9112345678",
-//   },
-//   {
-//     first_name: "Rajesh",
-//     last_name: "Singh",
-//     city: "Jaipur",
-//     contact_number: "9907654321",
-//   },
-//   {
-//     first_name: "Rajesh",
-//     last_name: "Yadav",
-//     city: "Lucknow",
-//     contact_number: "9812345678",
-//   },
-//   {
-//     first_name: "Ankita",
-//     last_name: "Patel",
-//     city: "Surat",
-//     contact_number: "9123459876",
-//   },
-//   {
-//     first_name: "Ankita",
-//     last_name: "Jain",
-//     city: "Chennai",
-//     contact_number: "9812341234",
-//   },
-//   {
-//     first_name: "Saurabh",
-//     last_name: "Shah",
-//     city: "Mumbai",
-//     contact_number: "9876541222",
-//   },
-//   {
-//     first_name: "Vikas",
-//     last_name: "Agarwal",
-//     city: "Delhi",
-//     contact_number: "9901234567",
-//   },
-//   {
-//     first_name: "Vikram",
-//     last_name: "Rao",
-//     city: "Bangalore",
-//     contact_number: "9123456543",
-//   },
-//   {
-//     first_name: "Sunil",
-//     last_name: "Mishra",
-//     city: "Kolkata",
-//     contact_number: "9123453212",
-//   },
-//   {
-//     first_name: "Sunita",
-//     last_name: "Joshi",
-//     city: "Pune",
-//     contact_number: "9876547890",
-//   },
-//   {
-//     first_name: "Suresh",
-//     last_name: "Nair",
-//     city: "Hyderabad",
-//     contact_number: "9909876543",
-//   },
-// ];
+const data = [
+  {
+    first_name: "Amit",
+    last_name: "Sharma",
+    city: "Mumbai",
+    contact_number: "9876543210",
+  },
+  {
+    first_name: "Amit",
+    last_name: "Verma",
+    city: "Delhi",
+    contact_number: "9123456789",
+  },
+  {
+    first_name: "Sandeep",
+    last_name: "Gupta",
+    city: "Bangalore",
+    contact_number: "9876549870",
+  },
+  {
+    first_name: "Sandeep",
+    last_name: "Kumar",
+    city: "Hyderabad",
+    contact_number: "9871234567",
+  },
+  {
+    first_name: "Priya",
+    last_name: "Mehta",
+    city: "Ahmedabad",
+    contact_number: "9874561230",
+  },
+  {
+    first_name: "Priya",
+    last_name: "Chopra",
+    city: "Pune",
+    contact_number: "9112345678",
+  },
+  {
+    first_name: "Rajesh",
+    last_name: "Singh",
+    city: "Jaipur",
+    contact_number: "9907654321",
+  },
+  {
+    first_name: "Rajesh",
+    last_name: "Yadav",
+    city: "Lucknow",
+    contact_number: "9812345678",
+  },
+  {
+    first_name: "Ankita",
+    last_name: "Patel",
+    city: "Surat",
+    contact_number: "9123459876",
+  },
+  {
+    first_name: "Ankita",
+    last_name: "Jain",
+    city: "Chennai",
+    contact_number: "9812341234",
+  },
+  {
+    first_name: "Saurabh",
+    last_name: "Shah",
+    city: "Mumbai",
+    contact_number: "9876541222",
+  },
+  {
+    first_name: "Vikas",
+    last_name: "Agarwal",
+    city: "Delhi",
+    contact_number: "9901234567",
+  },
+  {
+    first_name: "Vikram",
+    last_name: "Rao",
+    city: "Bangalore",
+    contact_number: "9123456543",
+  },
+  {
+    first_name: "Sunil",
+    last_name: "Mishra",
+    city: "Kolkata",
+    contact_number: "9123453212",
+  },
+  {
+    first_name: "Sunita",
+    last_name: "Joshi",
+    city: "Pune",
+    contact_number: "9876547890",
+  },
+  {
+    first_name: "Suresh",
+    last_name: "Nair",
+    city: "Hyderabad",
+    contact_number: "9909876543",
+  },
+];
 
 const SearchResults = () => {
-  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState(data || []);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const location = useLocation(); // Get the current location (URL)
